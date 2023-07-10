36e9cfcdce8ad1adba3bf9ed923fcf9c202ab8d6
Anastasia Kuzmina <kuzminaanastasya2000@gmail.com>
2023-07-10 12:50:41 +0400

    2-module

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,9 @@
 function isEmpty(obj) {
-  // ваш код...
+  result = 0;
+  for(let key in obj){
+    if (key === undefined){
+      result += 1
+    }
+  }
+  return result == Object.keys(obj).length
 }
