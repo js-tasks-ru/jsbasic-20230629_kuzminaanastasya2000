7870e512690a19b0ba0f6d8745d43a97e3ca9dec
Anastasia Kuzmina <kuzminaanastasya2000@gmail.com>
2023-07-14 19:19:08 +0400

    3-module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
-  // ваш код...
+  let f = str
+  .split("-")
+  .map(item => item.slice(0, 1).toUpperCase() + item.slice(1))
+  .join("")
+  return f.slice(0, 1).toLowerCase() + f.slice(1)
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+  let ageFilter = users.filter(item => item.age <= age)
+  let balance = ageFilter.map(item => item.balance);
+  let name = ageFilter.map(item => item.name);
+  return `${name[0]}, ${balance[0]}\n${name[1]}, ${balance[1]}`
 }
