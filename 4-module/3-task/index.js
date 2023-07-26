2749ae9489a720b6beec74de0ecd11217e5e28a7
Anastasia Kuzmina <kuzminaanastasya2000@gmail.com>
2023-07-26 17:43:46 +0400

    4-module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  let tr = table.querySelectorAll("tr");
+  for(i = 1; i < tr.length; i++){
+    if (table.rows[i].cells[3].getAttribute('data-available') == "true") {
+      tr[i].classList.add("available") ;
+    } else if (table.rows[i].cells[3].getAttribute('data-available') == undefined){
+      tr[i].setAttribute('hidden', true);
+    } else {
+      tr[i].classList.add("unavailable");
+    }
+  }
+  for(i = 1; i < tr.length; i++){
+    if (table.rows[i].cells[2].innerHTML == "f") {
+      tr[i].classList.add("female");
+    } else {
+      tr[i].classList.add("male");
+    }
+  }
+  for(i = 1; i < tr.length; i++){
+    if (table.rows[i].cells[1].innerHTML < 18) {
+      tr[i].style.textDecoration = 'line-through';
+    }
+  }
 }
