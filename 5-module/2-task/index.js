dcb5505473cfdbeb051b738bf25037791bcb4dc0
Anastasia Kuzmina <kuzminaanastasya2000@gmail.com>
2023-08-08 13:10:40 +0400

    module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let text = document.getElementById('text');
+  button.addEventListener('click', hiddenNohidden);
+  function hiddenNohidden () {
+    if (!text.hidden) {
+      text.hidden = true;
+    } else {
+      text.hidden = false;
+    }
+  }
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+  let carouselInner = document.querySelector('.carousel__inner');
+  let buttonNext = document.querySelector('.carousel__arrow_right');
+  let buttonBack = document.querySelector('.carousel__arrow_left');
+  let elem = document.querySelector('[data-carousel-holder]');
+  let currentSlideNumber = 0;
+  let slidesAmount = 4;
+  update()
+  elem.onclick = ({target}) => {
+    if(target.closest('.carousel__arrow_right')){
+      next()
+    }
+    if(target.closest('.carousel__arrow_left')){
+      back()
+    }
+  }
+  function next() {
+    currentSlideNumber++;
+    update()
+  }
+  function back() {
+    currentSlideNumber--;
+    update()
+  }
+  function update() {
+    let slideWidth = -carouselInner.offsetWidth * currentSlideNumber;
+    carouselInner.style.transform = `translateX(${slideWidth}px)`;
+    if (currentSlideNumber == slidesAmount - 1) {
+      buttonNext.style.display = 'none'
+    } else {
+      buttonNext.style.display = ''
+    }
+    if (currentSlideNumber == 0) {
+      buttonBack.style.display = 'none'
+    } else if (currentSlideNumber > 0){
+      buttonBack.style.display = ''
+    }
+  }
 }
+
