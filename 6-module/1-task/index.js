aa9a24bbd439253cfd1f1c0a20588d20601137fd
Anastasia Kuzmina <kuzminaanastasya2000@gmail.com>
2023-08-14 17:02:02 +0400

    6.1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,56 @@
  *
  */
 export default class UserTable {
-  constructor(rows) {
+  #rows;
+  #elem;
+  constructor(rows){
+    this.#rows = rows;
+    this.#elem = document.querySelector('table');
+    this.addRows();
+  }
+  get elem(){
+    return this.#elem;
+  }
+  addRows(){
+    let structure = `
+    <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>` + this.#rows.map(e => `
+    <tr>
+       <td>${e.name}</td>
+       <td>${e.age}</td>
+       <td>${e.salary}</td>
+       <td>${e.city}</td>
+       <td><button>X</button></td>
+    </tr>
+    `).join("") + `</tbody>`
+    this.#elem.innerHTML = structure;
+    for(let b of this.elem.querySelectorAll('button')){
+      b.addEventListener('click', this);
+    }
+  }
+  handleEvent(event){
+    let row = event.target.parentElement.parentElement;
+    this.#rows.splice(row.rowIndex - 1, 1);
+    row.remove()
   }
 }
+
+/*export default function makeTable(rows) {
+  let table = document.createElement("table");
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  let td2 = document.createElement("td");
+  td1.innerHTML = 'fkfkf';
+  td2.innerHTML = 'f[f[]]';
+  tr.append(td1);
+  tr.append(td2);
+  table.append(tr);
+}*/
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,3 @@
+import createElement from '../../assets/lib/create-element.js';
 export default class ProductCard {
-  constructor(product) {
-  }
 }
